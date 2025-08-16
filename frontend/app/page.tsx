b5a9fa79990d501737832403a020b04cb6d20f87
Moriaque <moriaque23@gmail.com>
2025-08-16 20:51:13 +0100

    feat(ui integration): subhero section set up and responsive

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import HomePageHero from "@/components/HomepageHero";
+import SubheroSection from "@/components/SubheroSection";
 
 export default function Home() {
   return (
     <section>
       <Header />
       <HomePageHero />
+      <SubheroSection />
     </section>
   );
 }
