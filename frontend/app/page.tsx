df2851a9b7972890cfc60c4b828d6ca5b3d00605
Moriaque <moriaque23@gmail.com>
2025-08-16 15:15:39 +0100

    feat(ui integration): assets folder created and homepage herosection ui set up

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import HomePageHero from "@/components/HomepageHero";
 
 export default function Home() {
   return (
     <section>
       <Header />
+      <HomePageHero />
     </section>
   );
 }
