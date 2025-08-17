c93a9fcf222088da0b948caec9f142e39a6eb87c
Moriaque <moriaque23@gmail.com>
2025-08-17 10:10:07 +0100

    feat(ui integration): howitworks section over but not responsive

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,15 @@
 import Header from "@/components/Header";
 import HomePageHero from "@/components/HomepageHero";
+import HowItWorksSection from "@/components/HowItWorksSection";
 import SubheroSection from "@/components/SubheroSection";
 
 export default function Home() {
   return (
-    <section>
+    <section className="">
       <Header />
       <HomePageHero />
       <SubheroSection />
+      <HowItWorksSection />
     </section>
   );
 }
