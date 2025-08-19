477b5465d026bf067584250617bded9e0d2e88ef
Moriaque <moriaque23@gmail.com>
2025-08-20 00:21:25 +0100

    feat(ui integration): project submission form step ui created

---
+++ frontend/app/submit/page.tsx
@@ -0,0 +1,11 @@
+import SubmitHeroSection from "@/components/SubmitHeroSection";
+import SubmitProjectFormContainer from "@/components/SubmitProjectFormContainer";
+
+export default function page() {
+  return (
+    <section>
+      <SubmitHeroSection />
+      <SubmitProjectFormContainer />
+    </section>
+  );
+}
---
+++ frontend/components/PhoneNumberField.tsx
@@ -0,0 +1,19 @@
+type Props = {
+  label: string;
+  placeholder?: string;
+};
+
+export default function PhoneNumberField({ label, placeholder }: Props) {
+  return (
+    <div>
+      <label className="block text-gray-800 text-lg font-medium">
+        <span>{label}</span> <span className="text-red-500">*</span>
+      </label>
+      <input
+        type="email"
+        placeholder={placeholder}
+        className="border-none outline-none bg-gray-100 px-2 py-3 rounded-md w-full"
+      />
+    </div>
+  );
+}
