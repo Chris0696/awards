d724cda2296e4b889d7daa18be572fbac875de6e
Moriaque <moriaque23@gmail.com>
2025-08-20 23:47:39 +0100

    feat(ui integration): create the modals

---
+++ frontend/components/LoginModal.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import EmailField from "./EmailField";
+import PasswordField from "./PasswordField";
+import Popover from "./Popover";
+import { ChevronRightIcon } from "lucide-react";
+
+type LoginModalProps = {
+  showModal: boolean;
+  setShowModal: (show: boolean) => void;
+};
+
+export default function LoginModal({
+  showModal,
+  setShowModal,
+}: LoginModalProps) {
+  return (
+    <Popover
+      title="Je vote"
+      visible={showModal}
+      onClose={() => setShowModal(false)}
+      isLogin
+    >
+      <div className="w-4/5 mx-auto mt-10 mb-20 space-y-4">
+        <div className="flex justify-center flex-col items-center">
+          <h2 className="text-4xl font-bold">Bienvenu!</h2>
+          <p>Connectez vous à votre espace</p>
+        </div>
+        <div className="space-y-2">
+          <EmailField label="Adresse email" placeholder="Email" />
+          <div>
+            <PasswordField label="Mot de passe" placeholder="Mot de passe" />
+            <Link href={""} className="text-secondary">
+              Mot de passe oublié ?
+            </Link>
+          </div>
+        </div>
+        <div className="mt-10">
+          <button className="flex items-center justify-center space-x-2 bg-secondary text-white px-4 py-2 rounded-md w-full cursor-pointer">
+            <span>Se connecter</span>
+            <ChevronRightIcon />
+          </button>
+        </div>
+      </div>
+    </Popover>
+  );
+}
---
+++ frontend/components/MakeVoteModal.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import Popover from "./Popover";
+import PhoneNumberField from "./PhoneNumberField";
+import NumberField from "./NumberField";
+import { ChevronRightIcon } from "lucide-react";
+import ArtworkImg from "@/assets/artworklight.jpg";
+
+type Props = {
+  showModal: boolean;
+  setShowModal: (show: boolean) => void;
+};
+export default function MakeVoteModal({ showModal, setShowModal }: Props) {
+  return (
+    <Popover
+      title="Je vote pour le projet “Kit Solaire Mobile”"
+      visible={showModal}
+      onClose={() => setShowModal(false)}
+    >
+      <div className="w-4/5 mx-auto mt-10 mb-20  ">
+        <div className="grid grid-cols-1 md:grid-cols-2 bg-gray-200 p-4 w-[85%] mx-auto rounded-lg">
+          <div>
+            <h4>
+              <span>Catégorie: </span>
+              <span>Énergie</span>
+            </h4>
+            <h4>
+              <span>Auteur: </span>
+              <span>Mireile Assaba</span>
+            </h4>
+            <h4>
+              <span>Votes actuels: </span>
+              <span>248 votes</span>
+            </h4>
+          </div>
+          <div>
+            <Image
+              src={ArtworkImg}
+              alt="Project image"
+              className="w-[85%] md:mx-auto rounded-lg h-20 object-cover"
+            />
+          </div>
+        </div>
+        <div className="mt-12 md:space-y-4">
+          <PhoneNumberField label="Numéro" placeholder="Ex:0161000000" />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <NumberField label="Nombre de vote" placeholder="2" />
+
+            <NumberField label="Montant(XOF)" placeholder="200" />
+          </div>
+          <div className="mt-10">
+            <button className="flex items-center justify-center space-x-2 bg-secondary text-white px-4 py-2 rounded-md w-full cursor-pointer">
+              <span>Se connecter</span>
+              <ChevronRightIcon />
+            </button>
+          </div>
+        </div>
+      </div>
+    </Popover>
+  );
+}
