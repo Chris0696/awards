839c426a776eb37af899d23557654523a9b1145a
Chris0696 <oswaldjchris@gmail.com>
2025-09-06 14:16:01 +0100

    api route changed

--- backend/awards/urls.py
+++ backend/awards/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
     
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
