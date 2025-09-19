3c0ad72d9f6503393c7ae3af1d0799ba7fb3520c
Moriaque <moriaque23@gmail.com>
2025-09-19 23:08:32 +0100

    remove fedapay collect route

--- frontend/app/api/fedapay/webhook/route.ts
+++ frontend/app/api/fedapay/webhook/route.ts
@@ -5,19 +5,52 @@ export async function POST(req: NextRequest) {
     const event = await req.json();
 
     if (event.object === "transaction") {
+      const context = event.entity.custom_metadata.context; //would be project-awards
       const payload = {
-        reference: event.entity.reference,
-        status: event.entity.status,
-        context: event.entity.custom_metadata.context, //would be project-awards
-        projectId: event.entity.custom_metadata.projectId,
-        voteCount: event.entity.custom_metadata.voteCount,
-        fullname: event.entity.customer.firstname,
+        payment_reference: event.entity.reference,
+        payment_status: event.entity.status,
+        project_id: event.entity.custom_metadata.projectId,
+        vote_count: event.entity.custom_metadata.voteCount,
+        voter_name: event.entity.customer.firstname,
         email: event.entity.customer.email,
-        phoneNumber: event.entity.payment_method?.number,
-        transactionKey: event.entity.transaction_key,
+        country_code: "+229",
+        phone: "0161117500",
+        //phone: event.entity.payment_method?.number,
+        external_transaction_id: event.entity.transaction_key,
       };
+      if (context === "") {
+        try {
+          const response = await fetch(
+            `${process.env.API_URL}votes/create-and-pay/`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(payload),
+            }
+          );
+          const contentType = response.headers.get("content-type");
 
-      // TODO: marquer la commande comme payée dans ta DB
+          let data: any;
+          if (contentType && contentType.includes("application/json")) {
+            data = await response.json();
+          } else {
+            data = await response.text();
+          }
+
+          if (!response.ok) {
+            return NextResponse.json(
+              { error: data },
+              { status: response.status }
+            );
+          }
+
+          return NextResponse.json(data, { status: 201 });
+        } catch (error: any) {
+          return NextResponse.json({ error: error.message }, { status: 500 });
+        }
+      }
     }
 
     return NextResponse.json({ received: true }, { status: 200 });
