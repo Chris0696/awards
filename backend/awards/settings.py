2c3c7955c0522cfb27b77c19b91ccee369f3efb9
Chris0696 <oswaldjchris@gmail.com>
2025-09-29 09:22:29 +0100

    Some cookie parameters added to the backend

--- backend/awards/settings.py
+++ backend/awards/settings.py
@@ -61,6 +61,11 @@ else:
 # Configuration HTTPS
 # SECURE_SSL_REDIRECT = True
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
     
 # Application definition
 
