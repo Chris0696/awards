8ea511d4dc5e63d757e409ced91bd68a55e8353d
Chris0696 <oswaldjchris@gmail.com>
2025-09-29 08:30:42 +0100

    Django admin url modified

--- backend/awards/urls.py
+++ backend/awards/urls.py
@@ -23,7 +23,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('djadmin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
     
     path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
